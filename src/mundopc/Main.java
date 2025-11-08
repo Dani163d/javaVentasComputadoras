151d836f80a6400554dbeed1f79860c12f2def2e
Dani163d <Danielasilvacastillo163@gmail.com>
2025-11-08 17:17:55 -0400

    se creo la clase Computadora

--- src/mundopc/Main.java
+++ src/mundopc/Main.java
@@ -1,5 +1,6 @@
 package mundopc;
 
+import mundopc.modelo.Computadora;
 import mundopc.modelo.Monitor;
 import mundopc.modelo.Raton;
 import mundopc.modelo.Teclado;
@@ -11,11 +12,13 @@ public class Main {
         //crear objetos
         Raton ratonLenovo = new Raton("bluetooth", "lenovo");
         System.out.println(ratonLenovo);
-
         Teclado tecladoLenovo = new Teclado("Bluetooth", "lenovo");
         System.out.println(tecladoLenovo);
-
         Monitor monitorLenovo = new Monitor("lenovo", 32);
         System.out.println(monitorLenovo);
+        //creamos in pbjeto de tipo computadora
+        Computadora computadoraLenovo =
+                new Computadora("Computadora Lenovo", monitorLenovo, tecladoLenovo, ratonLenovo);
+        System.out.println(computadoraLenovo);
     }
 }
